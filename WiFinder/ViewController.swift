12366764390285e98c60b216681fc00060af7bf6
kerkcw <kerkcw@m1.com.sg>
2018-12-19 22:56:53 +0800

    first commit

---
+++ WiFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiFinder
+//
+//  Created by Kerk Chin Wee on 19/12/18.
+//  Copyright © 2018 Chin Wee Kerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
