97a7e51c7ff325964ffa0915c0e23a4093047833
kerkcw <kerkcw@m1.com.sg>
2018-12-20 23:05:11 +0800

    Remove API testing

--- WiFinder/ViewModels/ITunesSearchViewModel.swift
+++ WiFinder/ViewModels/ITunesSearchViewModel.swift
@@ -9,25 +9,46 @@
 import RxSwift
 import RxCocoa
 
-class ITunesSearchViewModel:  NSObject {
+class ITunesSearchViewModel: NSObject {
   private let disposeBag = DisposeBag()
   private let model = ITunesSearchManager()
   private let searchResults: BehaviorRelay<[ITunesSearchResult]>
+  private let viewController: ITunesSearchViewController
+  
+  public var mediaType: ITunesSearchMedia = .music
   
   init(viewController: ITunesSearchViewController) {
     self.searchResults = BehaviorRelay<[ITunesSearchResult]>(value: [])
+    self.viewController = viewController
     super.init()
-    self.search()
+    if let tabBarController = viewController.tabBarController {
+      self.addObserver(tabBarController, forKeyPath: "selectedIndex", options: [.new], context: nil)
+    }
   }
   
-  private func search() {
-    self.model.search(hints: ["jackson"], media: .music)?
-      .map({ (results) -> [ITunesSearchResult] in
-        results.forEach({ (result) in
-          print("\(result.name): \(result.description)")
-        })
-        return results
-      })
+  deinit {
+    if let tabBarController = viewController.tabBarController {
+      self.removeObserver(tabBarController, forKeyPath: "selectedIndex")
+    }
+  }
+  
+  override func observeValue(
+    forKeyPath keyPath: String?,
+    of object: Any?, change: [NSKeyValueChangeKey : Any]?,
+    context: UnsafeMutableRawPointer?
+  ) {
+    if let key = keyPath, key == "selectedIndex" {
+      switch change?[NSKeyValueChangeKey.newKey] as? Int {
+        case .some(0): self.mediaType = .music
+        case .some(1): self.mediaType = .movie
+        case .some(2): self.mediaType = .tvShow
+        default: break
+      }
+    }
+  }
+  
+  private func search(hints: [String]) {
+    self.model.search(hints: hints, media: self.mediaType)?
       .bind(to: self.searchResults)
       .disposed(by: self.disposeBag)
   }
